42fb77d96e194298ed19e8aaf5088cb0c8b3757f
Punitha <punitha.lakshmi87@gmail.com>
2022-08-25 12:27:07 -0700

    footer updated

--- app/Views/frontend/footer.php
+++ app/Views/frontend/footer.php
@@ -123,36 +123,36 @@
     <a href="#" data-target="html" class="scroll-to-target scroll-to-top"><i class="fa fa-angle-up"></i></a>
 
 
-    <script src="<?=base_url();?>/frontend/<?=base_url();?>/frontend/assets/vendors/jquery/jquery-3.6.0.min.js"></script>
-    <script src="<?=base_url();?>/frontend/<?=base_url();?>/frontend/assets/vendors/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <script src="<?=base_url();?>/frontend/<?=base_url();?>/frontend/assets/vendors/jarallax/jarallax.min.js"></script>
-    <script src="<?=base_url();?>/frontend/<?=base_url();?>/frontend/assets/vendors/jquery-ajaxchimp/jquery.ajaxchimp.min.js"></script>
-    <script src="<?=base_url();?>/frontend/<?=base_url();?>/frontend/assets/vendors/jquery-appear/jquery.appear.min.js"></script>
-    <script src="<?=base_url();?>/frontend/<?=base_url();?>/frontend/assets/vendors/jquery-circle-progress/jquery.circle-progress.min.js"></script>
-    <script src="<?=base_url();?>/frontend/<?=base_url();?>/frontend/assets/vendors/jquery-magnific-popup/jquery.magnific-popup.min.js"></script>
-    <script src="<?=base_url();?>/frontend/<?=base_url();?>/frontend/assets/vendors/jquery-validate/jquery.validate.min.js"></script>
-    <script src="<?=base_url();?>/frontend/<?=base_url();?>/frontend/assets/vendors/nouislider/nouislider.min.js"></script>
-    <script src="<?=base_url();?>/frontend/<?=base_url();?>/frontend/assets/vendors/odometer/odometer.min.js"></script>
-    <script src="<?=base_url();?>/frontend/<?=base_url();?>/frontend/assets/vendors/swiper/swiper.min.js"></script>
-    <script src="<?=base_url();?>/frontend/<?=base_url();?>/frontend/assets/vendors/tiny-slider/tiny-slider.min.js"></script>
-    <script src="<?=base_url();?>/frontend/<?=base_url();?>/frontend/assets/vendors/wnumb/wNumb.min.js"></script>
-    <script src="<?=base_url();?>/frontend/<?=base_url();?>/frontend/assets/vendors/wow/wow.js"></script>
-    <script src="<?=base_url();?>/frontend/<?=base_url();?>/frontend/assets/vendors/isotope/isotope.js"></script>
-    <script src="<?=base_url();?>/frontend/<?=base_url();?>/frontend/assets/vendors/countdown/countdown.min.js"></script>
-    <script src="<?=base_url();?>/frontend/<?=base_url();?>/frontend/assets/vendors/owl-carousel/owl.carousel.min.js"></script>
-    <script src="<?=base_url();?>/frontend/<?=base_url();?>/frontend/assets/vendors/bxslider/jquery.bxslider.min.js"></script>
-    <script src="<?=base_url();?>/frontend/<?=base_url();?>/frontend/assets/vendors/bootstrap-select/js/bootstrap-select.min.js"></script>
-    <script src="<?=base_url();?>/frontend/<?=base_url();?>/frontend/assets/vendors/vegas/vegas.min.js"></script>
-    <script src="<?=base_url();?>/frontend/<?=base_url();?>/frontend/assets/vendors/jquery-ui/jquery-ui.js"></script>
-    <script src="<?=base_url();?>/frontend/<?=base_url();?>/frontend/assets/vendors/timepicker/timePicker.js"></script>
-    <script src="<?=base_url();?>/frontend/<?=base_url();?>/frontend/assets/vendors/circleType/jquery.circleType.js"></script>
-    <script src="<?=base_url();?>/frontend/<?=base_url();?>/frontend/assets/vendors/circleType/jquery.lettering.min.js"></script>
+    <script src="<?=base_url();?>/frontend/assets/vendors/jquery/jquery-3.6.0.min.js"></script>
+    <script src="<?=base_url();?>/frontend/assets/vendors/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="<?=base_url();?>/frontend/assets/vendors/jarallax/jarallax.min.js"></script>
+    <script src="<?=base_url();?>/frontend/assets/vendors/jquery-ajaxchimp/jquery.ajaxchimp.min.js"></script>
+    <script src="<?=base_url();?>/frontend/assets/vendors/jquery-appear/jquery.appear.min.js"></script>
+    <script src="<?=base_url();?>/frontend/assets/vendors/jquery-circle-progress/jquery.circle-progress.min.js"></script>
+    <script src="<?=base_url();?>/frontend/assets/vendors/jquery-magnific-popup/jquery.magnific-popup.min.js"></script>
+    <script src="<?=base_url();?>/frontend/assets/vendors/jquery-validate/jquery.validate.min.js"></script>
+    <script src="<?=base_url();?>/frontend/assets/vendors/nouislider/nouislider.min.js"></script>
+    <script src="<?=base_url();?>/frontend/assets/vendors/odometer/odometer.min.js"></script>
+    <script src="<?=base_url();?>/frontend/assets/vendors/swiper/swiper.min.js"></script>
+    <script src="<?=base_url();?>/frontend/assets/vendors/tiny-slider/tiny-slider.min.js"></script>
+    <script src="<?=base_url();?>/frontend/assets/vendors/wnumb/wNumb.min.js"></script>
+    <script src="<?=base_url();?>/frontend/assets/vendors/wow/wow.js"></script>
+    <script src="<?=base_url();?>/frontend/assets/vendors/isotope/isotope.js"></script>
+    <script src="<?=base_url();?>/frontend/assets/vendors/countdown/countdown.min.js"></script>
+    <script src="<?=base_url();?>/frontend/assets/vendors/owl-carousel/owl.carousel.min.js"></script>
+    <script src="<?=base_url();?>/frontend/assets/vendors/bxslider/jquery.bxslider.min.js"></script>
+    <script src="<?=base_url();?>/frontend/assets/vendors/bootstrap-select/js/bootstrap-select.min.js"></script>
+    <script src="<?=base_url();?>/frontend/assets/vendors/vegas/vegas.min.js"></script>
+    <script src="<?=base_url();?>/frontend/assets/vendors/jquery-ui/jquery-ui.js"></script>
+    <script src="<?=base_url();?>/frontend/assets/vendors/timepicker/timePicker.js"></script>
+    <script src="<?=base_url();?>/frontend/assets/vendors/circleType/jquery.circleType.js"></script>
+    <script src="<?=base_url();?>/frontend/assets/vendors/circleType/jquery.lettering.min.js"></script>
 
 
 
 
     <!-- template js -->
-    <script src="<?=base_url();?>/frontend/<?=base_url();?>/frontend/assets/js/insur.js"></script>
+    <script src="<?=base_url();?>/frontend/assets/js/insur.js"></script>
 </body>
 
 </html>
