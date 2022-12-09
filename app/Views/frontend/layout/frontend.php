f3c3cdc735db4e29615cfed0856769ffdb804cb7
Punitha <punitha.lakshmi87@gmail.com>
2022-12-09 06:35:03 +0530

    -

--- app/Views/frontend/layout/frontend.php
+++ app/Views/frontend/layout/frontend.php
@@ -1,8 +1,10 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <?=csrf_meta();?>
     <title>Home - Sunpharma</title>
 <meta name="description" content="Sun Pharma Science Foundation Research Awards 2022
 Online Submission Of Nominations">
