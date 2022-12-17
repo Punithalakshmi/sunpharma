c4b6b9db9b8d6b8d15fe2ced3e807e0abe91e8f6
Sathish <sathish@izaaptech.in>
2022-12-18 00:59:38 +0530

    Admin end fixes

--- app/Views/admin/event_registeration/list.php
+++ app/Views/admin/event_registeration/list.php
@@ -50,8 +50,8 @@
                           <td><?=(isset($user['phone']) && ($user['phone']!='2147483647'))?$user['phone']:"-";?></td>
                           <td><?=$user['registeration_no'];?></td>
                           <td>
-                            <a href="<?=base_url().'/admin/eventregisteration/add/'.$user['id'];?>" class="btn btn-info btn-xs"><i class="fa fa-pencil"></i>  </a>
-                            <a onclick="userDelete('registration','<?=$user['id'];?>','/admin/eventregisteration/delete')" class="btn btn-danger btn-xs"><i class="fa fa-trash-o"></i>  </a>
+                            <a href="<?=base_url().'/admin/eventregisteration/add/'.$user['id'];?>" class="btn btn-info btn-xs"><i class="fa fa-pencil"></i> Edit</a>
+                            <a onclick="userDelete('registration','<?=$user['id'];?>','/admin/eventregisteration/delete')" class="btn btn-danger btn-xs"><i class="fa fa-trash-o"></i> Delete</a>
                           </td>
                         </tr>
                         <?php endforeach;
