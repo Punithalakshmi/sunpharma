cf7f59de7173898383d144abb37ac4e4b2184c98
Sathish <sathish@izaaptech.in>
2023-04-06 14:31:24 +0530

    Design fixes - mapping page fixes

--- app/Views/admin/jury/mapping.php
+++ app/Views/admin/jury/mapping.php
@@ -1,102 +1,148 @@
 <div class="right_col" role="main" style="min-height: 246px;">
-          <div class="">
-            <div class="page-title">
-              <div class="title_left">
-                <h3>Jury mapping with Awards</h3>
-              </div>
+  <div class="">
+    <div class="page-title">
+      <div class="title_left ml-3" style="width:100%;">
+        <h3>Jury mapping with Awards</h3>
+      </div>
+    </div>
+
+    <div class="clearfix"></div>
+    <form method="POST" name="jury_mapping" class="form-horizontal" action="<?= base_url(); ?>/admin/jury/mapping">
+      <?= csrf_field(); ?>
+      <div class="d-flex flex-column">
+        <div class="col-md-12 d-flex justify-content-center align-items-center flex-column">
+
+          <div class="jurymap x_panel" style="min-height:400px;">
+            <div class="jurymaplist">
+              <label>Jury :</label>
+
+              <select class="selectpicker" name="jury">
+                <?php if (count($juryLists)) :
+                  foreach ($juryLists as $jkey => $jvalue) : ?>
+                    <option value="<?= $jvalue['id']; ?>"><?= $jvalue['firstname'] . ' ' . $jvalue['lastname']; ?></option>
+                <?php endforeach;
+                endif; ?>
+              </select>
+            </div>
+
+            <div class="jurymaplist">
+              <label>Award Type* :</label>
+              <select class="selectpicker" multiple name="award[]">
+                <?php if (count($awardLists)) :
+                  foreach ($awardLists as $akey => $avalue) : ?>
+                    <option value="<?= $avalue['id']; ?>"><?= $avalue['title']; ?></option>
+                <?php endforeach;
+                endif; ?>
+              </select>
+            </div>
+
+            <div class="jurymaplist">
+              <label></label> <input type="submit" class="btn btn-success" name="jurymap_submit" value="ASSIGN">
             </div>
 
             <div class="clearfix"></div>
-            <form method="POST" name="jury_mapping" class="form-horizontal" action="<?=base_url();?>/admin/jury/mapping">
-                <?= csrf_field(); ?>
-                <div class="row justify-content-center">
-                    <div class="col-md-6 col-lg-4 d-flex justify-content-center align-items-center">
-                       
-                        <select class="selectpicker" name="jury">
-                        <?php if(count($juryLists)): 
-                                foreach($juryLists as $jkey=>$jvalue): ?>
-                                <option value="<?=$jvalue['id'];?>"><?=$jvalue['firstname'].' '.$jvalue['lastname'];?></option>
-                         <?php endforeach; endif;?>        
-                        </select>
-                    </div>
-                    
-                    <div class="col-md-6 col-lg-4 d-flex justify-content-center align-items-center">
-            <div class="row">
-              <div class="col-md-12 col-sm-12 col-xs-12">
-                <div class="x_panel minhight">
-                <style>
-                 .jurymapping .bootstrap-select .dropdown-menu.inner {
-                    font-size: 16px;
-                    }
-                    .jurymapping .btn {
-                        border: 1px solid;
-                    }
-                    .jurymapping .bootstrap-select {
-                        width: 100%!important;
-                    }
-                    .bootstrap-select.show-tick .dropdown-menu .selected span.check-mark{color: #f7941e;}
-                    .dropdown-item.active, .dropdown-item:active {
-                        background: #047cb2;
-                    }
-                    .minhight{min-height:580px;}
-                </style>
-                  <div class="x_content jurymapping ">
-                    <br>
-                   
-                                            <div class="form-group">
-                        <label class="control-label col-md-3 col-sm-3 col-xs-12 pt10" for="first-name">Award Type <span class="required">*</span>
-                        </label>
-                        <div class="col-md-6 col-sm-6 col-xs-12">
-                        <select class="selectpicker" multiple name="award[]" >
-                        <?php if(count($awardLists)): 
-                                foreach($awardLists as $akey=>$avalue): ?>
-                                <option value="<?=$avalue['id'];?>"><?=$avalue['title'];?></option>
-                         <?php endforeach; endif;?>       
-                        </select>
-                        </div>
-                      </div>
-                      <div class="clearfix"></div>
-<br>
-                      
-                      <div class="row justify-content-center">
-                      
-                      <div class="col-md-6 col-lg-4 d-flex justify-content-center align-items-center">
-                        <?php if(isset($validation) && $validation->getError('jury')) {?>
-                            <div class='alert alert-danger mt-2'>
-                                <?= $error = $validation->getError('jury'); ?>
-                            </div>
-                        <?php }?>
-                      </div>
-                      <div class="col-md-6 col-lg-4 d-flex justify-content-center align-items-center">
-                        <?php if(isset($validation) && $validation->getError('award')) {?>
-                            <div class='alert alert-danger mt-2'>
-                                <?= $error = $validation->getError('award'); ?>
-                            </div>
-                        <?php }?>
-                      </div> 
-                </div>
-                     
-                      <div class="ln_solid"></div>
-                        <div class="form-group">
-                          <div class="col-md-6 col-sm-6 col-xs-12 col-md-offset-3">
-                          <input type="submit" class="btn btn-success" name="jurymap_submit" value="ASSIGN">
-                            
-                          </div>
-                        </div>
 
-             
+
+            <div class="errormsg justify-content-center flex-column">
+
+              <div class="col-md-12 d-flex justify-content-center align-items-center">
+                <?php if (isset($validation) && $validation->getError('jury')) { ?>
+                  <div class='alert alert-danger mt-2'>
+                    <?= $error = $validation->getError('jury'); ?>
                   </div>
-                </div>
+                <?php } ?>
               </div>
-            </div></form>
+              <div class="col-md-12 d-flex justify-content-center align-items-center">
+                <?php if (isset($validation) && $validation->getError('award')) { ?>
+                  <div class='alert alert-danger mt-2'>
+                    <?= $error = $validation->getError('award'); ?>
+                  </div>
+                <?php } ?>
+              </div>
+            </div>
+
+          </div>
+
+
+
+          <style>
+            .jurymapping .bootstrap-select .dropdown-menu.inner {
+              font-size: 16px;
+            }
+
+            .jurymap .bootstrap-select .dropdown-toggle .filter-option-inner-inner {
+              font-size: 14px;
+            }
+
+            .jurymapping .btn {
+              border: 1px solid;
+            }
+
+            .jurymapping .bootstrap-select {
+              width: 100% !important;
+            }
+
+            .bootstrap-select.show-tick .dropdown-menu .selected span.check-mark {
+              color: #f7941e;
+            }
+
+            .dropdown-item.active,
+            .dropdown-item:active {
+              background: #047cb2;
+            }
+
+            .minhight {
+              min-height: 580px;
+            }
+
+            .jurymap {
+              padding-top: 20px;
+              display: flex;
+              flex-direction: column;
+              align-items: center;
+              justify-content: center;
+            }
+
+            .jurymap label {
+              min-width: 140px;
+              font-size: 16px;
+              color: black;
+              font-weight: 500;
+            }
+
+            .jurymap .jurymaplist {
+              margin-bottom: 10px;
+              width: 100%;
+              margin-top: 10px;
+              max-width: 460px;
+            }
+            .jurymap .bootstrap-select:not([class*=col-]):not([class*=form-control]):not(.input-group-btn) {
+              width: 300px;
+              }
+
+            .jurymap span.text {
+              font-size: 14px;
+              padding: 3px;
+            }
+
+            .bootstrap-select .dropdown-toggle .filter-option {
+              border: 1px solid #888;
+              border-radius: 4px;
+            }
+          </style>
+
+
+
+        </div>
+
+
+
+
+    </form>
 
     <!-- Footer Menu -->
-    
-      
-
-    </div>
-    </div>
 
 
 
-
+  </div>
+</div>
